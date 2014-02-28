54417357ccd85bd5961754fc9e505cb47a3f32d6
Benji Hooper <benji@sixp.in>
2014-02-28 13:27:43 +0000

    [US002] Integrated the DocumentBuilder and RouteReader together. Now you can build a HTML file.

--- src/main/java/com/benjih/caraml/Caraml.java
+++ src/main/java/com/benjih/caraml/Caraml.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.jsoup.nodes.Document;
+
 public class Caraml {
 
 	public static void main(String args[]) throws IOException {
@@ -16,6 +19,10 @@ public class Caraml {
 		List<Route> routes = RouteReader.readRoutes(projectRoot.getAbsolutePath() + "/config/routes");
 		
 		System.out.println(routes.size() + " routes found");
+		
+		Document document = new DocumentBuilder().addRoutes(routes).build();
+		
+		FileUtils.writeStringToFile(new File(uri + "/docs/routes.html"), document.toString());
 	}
 	
 }
--- src/test/java/com/benjih/caraml/CaramlTest.java
+++ src/test/java/com/benjih/caraml/CaramlTest.java
@@ -3,11 +3,14 @@ package com.benjih.caraml;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,26 +21,46 @@ public class CaramlTest {
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
 	@Before
-	public void before() {
+	public void before() throws IOException {
 	    System.setOut(new PrintStream(outContent));
 	    System.setErr(new PrintStream(errContent));
+	    
+	    FileUtils.deleteDirectory(new File("src/test/resources/project-1/docs"));
 	}
 
 	@After
-	public void after() {
+	public void after() throws IOException {
 	    System.setOut(null);
 	    System.setErr(null);
+	    
+	    FileUtils.deleteDirectory(new File("src/test/resources/project-1/docs"));
 	}
 	
 	@Test
-	public void iCanRunCaraml() throws IOException {
+	public void iCanRunCaraml() {
 		String[] args = {"src/test/resources/project-1"};
-		Caraml.main(args);
+		try {
+			Caraml.main(args);
+		} catch (IOException e1) {
+		}
 		
 		System.out.println(outContent.toString());
 		
 		assertTrue(outContent.toString().contains("Documenting project-1"));
 		assertTrue(outContent.toString().contains("3 routes found"));
+		
+		String writtenFile = null;
+		
+		try {
+			writtenFile = FileUtils.readFileToString(new File("src/test/resources/project-1/docs/routes.html"));
+		} catch (IOException e) {
+			fail();
+		}
+		
+		assertTrue(writtenFile.contains("<h1>Routes</h1>"));
+		assertTrue(writtenFile.contains("GET /home MainController.home"));
+		assertTrue(writtenFile.contains("POST /submit MainController.post"));
+		assertTrue(writtenFile.contains("* /about MainController.about"));
 	}
 	
 }
